bcf7ea9aa6e3624d5c5cbcfd03c94f554c3ab8de
VenkataSaiTarun Munduri <153797175+vmunduri@users.noreply.github.com>
2026-07-11 14:48:28 -0400

    add requirements.txt, gitignore and sinusoidal denominator tensor, embeddings scalar

---
+++ src/models/layers.py
@@ -0,0 +1,18 @@
+import math
+import torch
+
+
+def scale_embeddings_by_sqrt_dmodel(embeddings, d_model):
+    return embeddings * math.sqrt(d_model)
+
+
+def compute_positional_div_term(d_model):
+    #Log, exp makes better use of GPU Parallization than Pow function
+    # The denominator of sin(wt) or cost(wt) in sinusoidal positional encoding is 10000^(2i/d_model)
+    #we can convert it to exp(-i *  2 * log(10000) * (1/d_model))
+    #Because this is same for cos and sin, the constant (2 * log(1000) * (1/d_model)) can have shape of (d_model//2,)
+
+    frequency_divisor_const = torch.arange(d_model//2)
+    frequency_divisor_const = (frequency_divisor_const * math.exp(-2 * math.log(10000) * (1/d_model)))
+    return frequency_divisor_const
+
